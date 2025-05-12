4405a226406a3d7510f0a93470e5b3ff7e8a379c
Jason <jason.535220045@stu.untar.ac.id>
2025-05-12 18:10:43 +0700

    Fix 5

--- src/scripts/data/api.js
+++ src/scripts/data/api.js
@@ -48,7 +48,7 @@ export async function login({ email, password }) {
 
     localStorage.setItem("token", data.loginResult.token);
 
-    window.location.href = "/";
+    window.location.href = "/storiesapp/";
 
     return { success: true, message: data.message };
   } catch (error) {
